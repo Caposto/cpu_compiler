11f8157ff1f0c7699be1de789b483b949b5263f5
ChristianA <christiand.apostol@outlook.com>
2022-11-28 22:02:15 -0500

    first commit

---
+++ compile.py
@@ -0,0 +1,8 @@
+def translate_instruction(instruction):
+    return "00"
+
+# TODO: Write rule based translations to machine code
+# TODO: Store binary/hexidecimal reprsentations in a 2D array
+# TODO: Translate 2D array into text file of instructions
+
+# Read instructions from text file
